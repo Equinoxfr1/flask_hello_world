8349cf268f87b1b6fbed6e6f28e468fc00b9643c
Equinoxfr1 <124149634+Equinoxfr1@users.noreply.github.com>
2024-01-25 14:49:28 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
+
+@app.route('/fr/')
+def yolesgens():
+    return 'Bonjour les gens'
                                                                                                                                        
 if __name__ == "__main__":
   app.run(debug=True)
